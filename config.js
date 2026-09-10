525d817cf617c128ffc2cc6a66546a569a86eff0
bpalmeyro <bpalmeyro@argenprom.com.ar>
2026-09-10 17:36:55 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 const CONFIG = {
   // Pegá acá la URL que te da Apps Script al hacer "Implementar > Nueva implementación"
   // (termina en /exec). Ver paso 2 del README.
-  API_URL: "PEGAR_ACA_LA_URL_DEL_APPS_SCRIPT",
+  API_URL: https://script.google.com/macros/s/AKfycbzrgLwT9AQkY4VWUV1QcMih_xggk3jajbd0F5N4-EN39xvv1il1qGyTF2Balsdftzg/exec,
 
   // Código simple que vas a compartir con los operadores para entrar al dashboard.
   // No es un login real, es solo para que no quede 100% abierto al público.
